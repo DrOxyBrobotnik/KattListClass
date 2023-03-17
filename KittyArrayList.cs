d326d0b172f14aa7cf7b9642e8b1596eef021e01
DrOxyBrobotnik <120782522+DrOxyBrobotnik@users.noreply.github.com>
2023-03-17 14:33:49 +0100

    You got one week

---
+++ KittyArrayList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KattListClass
+{
+    public class KittyArrayList
+    {
+
+        public int Length { get; set; }
+        public void Add(int meow)
+        {
+            
+        }
+
+        public KittyArrayList(int dumma)
+        {
+
+        }
+
+        public KittyArrayList(int dumma, int bunna)
+        {
+
+        }
+
+        public KittyArrayList(int dumma, int bunna, int cumma)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace KattListClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new KittyArrayList(50, 100, 150);
+            list.Add(1);
+            
+
+
+        }
+    }
+}
